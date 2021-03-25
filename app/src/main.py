8845c8481f60b0322d546c14d058d728bc0aa538
jhkang <greennuri@gmail.com>
2021-03-25 23:47:34 +0900

    hugging face tokenizer와 연동 진행중

--- app/src/main.py
+++ app/src/main.py
@@ -9,7 +9,15 @@ from tqdm import tqdm
 from itertools import product, chain, combinations
 import traceback
 from multiprocessing.pool import ThreadPool
-from util import file_reader
+from util import file_reader, jsonp_reader
+
+from khaiii import KhaiiiApi, KhaiiiWord
+from konlpy.tag import Mecab
+
+from tokenizers import Tokenizer
+from tokenizers.trainers import BpeTrainer, WordPieceTrainer
+from tokenizers.models import BPE, WordPiece
+from tokenizers.pre_tokenizers import Whitespace, BertPreTokenizer, CharDelimiterSplit
 
 TRAIN_DIR = os.path.join(os.path.dirname(os.getcwd()), "trainset")
 
@@ -69,14 +77,26 @@ def list_to_iterables(data_list):
             yield element
 
 
-def generate_multi_generator():
+def generate_multi_generator(**kwargs):
     # train_dir
     # current_dir = os.getcwd()
     # base_dir = os.path.join(current_dir, train_dir)
+    fr = file_reader
+    if "file_type" in kwargs:
+        file_type = kwargs["file_type"]
+    else:
+        file_type = "txt"
+
+    if file_type == "json":
+        if "key" not in kwargs:
+            raise ValueError("must have key parameter")
+        else:
+            fr = jsonp_reader
+
     base_dir = TRAIN_DIR
 
     readers = [
-        file_reader(os.path.join(base_dir, file_name))
+        fr(file_name=os.path.join(base_dir, file_name), **kwargs)
         for file_name in os.listdir(base_dir)
     ]
 
@@ -89,19 +109,10 @@ def get_args():
     Returns:
         [type] -- [description]
     """
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("--data-path"", type=str, default="data")
-    # parser.add_argument("train_dir", type=str)
-    # parser.add_argument("n_cores", type=int, default=4)
-    # parser.add_argument("-ratio", type=float, default=0.2)
-    # parser.add_argument("-debug", default=False, action="store_true")
-    # args = parser.parse_args()
-    # return args
-
     # def get_args():
     """set CLI arguments"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data-path", type=str, default="data")
+    parser.add_argument("--data-path", type=str, default="../datasets/")
     parser.add_argument("--sample-rate", type=float, default=0.01)
     parser.add_argument(
         "--pretokenizer-type", type=str, choices=["khaiii", "mecab"], default="khaiii"
@@ -119,20 +130,79 @@ def get_args():
 
 def get_pretokenize_generator(morpheme_func):
     #  = None
-    readers = generate_multi_generator()
-
-    index = 0
-    for line in readers:
-        index = index + 1
-        print(f"line extract test : {line}")
-        if index > 5:
-            break
+    readers = generate_multi_generator(file_type="json", key="text")
+    count = 0
 
     for line in readers:
+        # count = count + 1
+        # if count > 10:
+        #     break
+
         if morpheme_func:
-            yield morpheme_func(line)
+            line = morpheme_func(line)
+            line_str = " ".join(
+                [
+                    element.lex if isinstance(element, KhaiiiWord) else element[0]
+                    for element in line
+                ]
+            )
+            # print(line_str)
+            yield line_str
         else:
             yield line
+
+
+def train_tokenizer(args):
+    """[summary]
+
+    Arguments:
+        args {[dictionary]} -- [arguments객체]
+    """
+    # Tokenizer train
+    if args.pretokenizer_type == "khaiii":
+        api = KhaiiiApi()
+        morpheme_func = api.analyze
+    elif args.pretokenizer_type == "mecab":
+        mecab = Mecab()
+        morpheme_func = mecab.morphs
+
+    # tokenizer-type", type=str, choices=["bbpe", "cbpe", "wp"], default="bbpe"
+    if args.tokenizer_type == "bbpe":
+        tokenizer = Tokenizer(BPE())
+        tokenizer.pre_tokenizer = BertPreTokenizer()
+        trainer = BpeTrainer(
+            special_tokens=args.special_tokens,
+            vocab_size=args.vocab_size,
+            min_frequency=args.min_frequency,
+        )
+    elif args.tokenizer_type == "cbpe":
+        tokenizer = Tokenizer(BPE())
+        tokenizer.pre_tokenizer = CharDelimiterSplit
+        trainer = BpeTrainer(
+            special_tokens=args.special_tokens,
+            vocab_size=args.vocab_size,
+            min_frequency=args.min_frequency,
+        )
+    elif agrs.tokenizer_type == "wp":
+        tokenizer = Tokenizer(WordPiece())
+        tokenizer.pre_tokenizer = Whitespace
+        trainer = WordPieceTrainer(
+            special_tokens=args.special_tokens,
+            vocab_size=args.vocab_size,
+            min_frequency=args.min_frequency,
+        )
+
+    # tokenizer.train()
+    tokenizer.train_from_iterator(
+        get_pretokenize_generator(morpheme_func), trainer=trainer
+    )
+    tokenizer.save("./vocab.bin")
+    test_string = "안녕하세요 이것은 테스트입니다"
+    output = tokenizer.encode(test_string)
+    print(output)
+    print(output.tokens)
+    print(output.type_ids)
+    print(output.offsets)
 
 
 def main():
@@ -142,46 +212,12 @@ def main():
     args = get_args()
     print(args)
 
-    print(TRAIN_DIR)
-
-    print()
-
     # trainset 생성
     generate_trainset(args.data_path, args.sample_rate)
 
     print("sampling complete")
 
-    if args.pretokenizer_type == "khaiii":
-        from khaiii import KhaiiiApi
-
-        api = KhaiiiApi()
-        morpheme_func = api.analyze
-
-    elif args.pretokenizer_type == "mecab":
-        from konlpy.tag import Mecab
-
-        mecab = Mecab()
-        morpheme_func = mecab.morphs
-
-    # pretokenizer 호출
-    # morpheme_func = None
-    count = 0
-    for line in get_pretokenize_generator(morpheme_func):
-        print(" ".join([element.lex for element in line]))
-        # # print(type(line))
-        # # print([element for element in line])
-        # for element in line:
-        #     print(element)
-        count = count + 1
-        if count > 5:
-            break
-    #     # print(get_pretokenize_generator(morpheme_func))
-    # print(morpheme_func("이것은 테스트입니다"))
-    # print()
-
-    # for element in api.analyze("이것은 테스트입니다"):
-    #     print(element)
-    #     print(type(element))
+    train_tokenizer(args)
 
 
 if __name__ == "__main__":
--- app/src/util.py
+++ app/src/util.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8
 
 
-def file_reader(file_name: str):
+def file_reader(file_name: str, **kwargs):
     """[summary]
 
     Arguments:
@@ -10,6 +10,25 @@ def file_reader(file_name: str):
     Returns:
         [str] -- [file에서 한줄 읽은 라인을 넘김]
     """
+
     # print(f"file name : {file_name}")
     for row in open(file_name, "r", encoding="utf-8"):
         yield row
+
+
+def jsonp_reader(file_name: str, **kwargs):
+    """[summary]
+
+    Arguments:
+        file_name {str} -- [description]
+        key {str} -- [description]
+    """
+    import json
+
+    if "key" not in kwargs:
+        raise ValueError("must have key parameter")
+
+    for row in open(file_name, "r", encoding="utf-8"):
+        line = json.loads(row)
+        yield line[kwargs["key"]]
+
